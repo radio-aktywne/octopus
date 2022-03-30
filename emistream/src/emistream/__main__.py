7a2cf9b7ebc21c8ec86cfe276ccd44077ddd7d65
Sebastian Pietras <spietras1998@gmail.com>
2022-03-30 23:07:32 +0200

    Improved CLI (#13)
    * Pinned click version
    
    * Improved click command

--- emistream/src/emistream/__main__.py
+++ emistream/src/emistream/__main__.py
@@ -4,9 +4,6 @@ This module provides basic CLI entrypoint.
 
 """
 
-import sys
-from typing import Optional
-
 import typer
 import uvicorn
 
@@ -21,10 +18,10 @@ def main(
         default="0.0.0.0", help="Host to run the server on"
     ),
     port: int = typer.Option(default=10000, help="Port to run the server on"),
-) -> Optional[int]:
+):
     """Command line interface for emistream."""
-    return uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host, port=port)
 
 
 if __name__ == "__main__":
-    sys.exit(cli())
+    cli()
