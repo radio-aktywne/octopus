8b30b061af09348a064b4a77a068afff31d5ebe5
Sebastian Pietras <spietras1998@gmail.com>
2022-03-30 23:13:00 +0200

    Added casting port to int (#14)

--- emistream/src/emistream/config.py
+++ emistream/src/emistream/config.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    port: int = os.getenv("EMISTREAM_PORT", 10000)
+    port: int = int(os.getenv("EMISTREAM_PORT", 10000))
     live_host: str = os.getenv("EMISTREAM_LIVE_HOST", "localhost")
-    live_port: int = os.getenv("EMISTREAM_LIVE_PORT", 9000)
+    live_port: int = int(os.getenv("EMISTREAM_LIVE_PORT", 9000))
     recording_host: str = os.getenv("EMISTREAM_RECORDING_HOST", "localhost")
-    recording_port: int = os.getenv("EMISTREAM_RECORDING_PORT", 31000)
+    recording_port: int = int(os.getenv("EMISTREAM_RECORDING_PORT", 31000))
 
 
 config = Config()
