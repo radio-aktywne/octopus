bf1487855af934ac41cf2af94ed8ca42ec29c0c4
Sebastian Pietras <me@spietras.dev>
2023-12-21 20:10:50 +0000

    Fetched template changes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,10 +1,18 @@
+import asyncio
+
 import pytest
+import pytest_asyncio
+from httpx import AsyncClient, BasicAuth
 from litestar import Litestar
 from litestar.testing import AsyncTestClient
 
 from emistream.api.app import AppBuilder
 from emistream.config.builder import ConfigBuilder
 from emistream.config.models import Config
+from tests.utils.containers import AsyncDockerContainer
+from tests.utils.waiting.conditions import CallableCondition
+from tests.utils.waiting.strategies import TimeoutStrategy
+from tests.utils.waiting.waiter import Waiter
 
 
 @pytest.fixture(scope="session")
@@ -21,8 +29,100 @@ def app(config: Config) -> Litestar:
     return AppBuilder(config).build()
 
 
-@pytest.fixture(scope="session")
-def client(app: Litestar) -> AsyncTestClient:
+@pytest_asyncio.fixture(scope="session")
+async def streamcast() -> AsyncDockerContainer:
+    """Streamcast container."""
+
+    async def _check() -> None:
+        auth = BasicAuth(username="admin", password="password")
+        async with AsyncClient(base_url="http://localhost:8000", auth=auth) as client:
+            response = await client.get("/admin/stats.json")
+            response.raise_for_status()
+
+    container = AsyncDockerContainer(
+        "ghcr.io/radio-aktywne/apps/streamcast:latest",
+        network="host",
+    )
+
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
+    async with container as container:
+        await waiter.wait()
+        yield container
+
+
+@pytest_asyncio.fixture(scope="session")
+async def fusion(streamcast: AsyncDockerContainer) -> AsyncDockerContainer:
+    """Fusion container."""
+
+    container = AsyncDockerContainer(
+        "ghcr.io/radio-aktywne/apps/fusion:latest",
+        network="host",
+    )
+
+    async with container as container:
+        await asyncio.sleep(5)
+        yield container
+
+
+@pytest_asyncio.fixture(scope="session")
+async def emiarchive() -> AsyncDockerContainer:
+    """Emiarchive container."""
+
+    async def _check() -> None:
+        async with AsyncClient(base_url="http://localhost:30000") as client:
+            response = await client.get("/minio/health/ready")
+            response.raise_for_status()
+
+    container = AsyncDockerContainer(
+        "ghcr.io/radio-aktywne/apps/emiarchive:latest",
+        network="host",
+    )
+
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
+    async with container as container:
+        await waiter.wait()
+        yield container
+
+
+@pytest_asyncio.fixture(scope="session")
+async def emirecorder(emiarchive: AsyncDockerContainer) -> AsyncDockerContainer:
+    """Emirecorder container."""
+
+    async def _check() -> None:
+        async with AsyncClient(base_url="http://localhost:31000") as client:
+            response = await client.get("/ping")
+            response.raise_for_status()
+
+    container = AsyncDockerContainer(
+        "ghcr.io/radio-aktywne/apps/emirecorder:latest",
+        network="host",
+    )
+
+    waiter = Waiter(
+        condition=CallableCondition(_check),
+        strategy=TimeoutStrategy(30),
+    )
+
+    async with container as container:
+        await waiter.wait()
+        yield container
+
+
+@pytest_asyncio.fixture(scope="session")
+async def client(
+    app: Litestar,
+    fusion: AsyncDockerContainer,
+    emirecorder: AsyncDockerContainer,
+) -> AsyncTestClient:
     """Reusable test client."""
 
-    return AsyncTestClient(app=app)
+    async with AsyncTestClient(app=app) as client:
+        yield client
--- tests/integration/test_available.py
+++ tests/integration/test_available.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
+import pytest
 from litestar.status_codes import HTTP_200_OK
 from litestar.testing import AsyncTestClient
 
 
+@pytest.mark.asyncio(scope="session")
 async def test_get(client: AsyncTestClient) -> None:
     """Test if GET /available returns correct response."""
 
-    async with client as client:
-        response = await client.get("/available")
+    response = await client.get("/available")
 
     assert response.status_code == HTTP_200_OK
 
